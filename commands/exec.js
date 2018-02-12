f5d780d8886beceb7c8fea148f3194370fb62b9d
PillGP <gaming.bot.discord@gmail.com>
2018-02-12 15:41:59 +0000

    :sparkles: | Changed eval, added exec & changed help

---
+++ commands/exec.js
@@ -0,0 +1,19 @@
+const exec = require("util").promisify(require("child_process").exec);
+const Discord = require('discord.js');
+
+exports.run = (client, msg, args) => {
+    const input = args.join(" ");
+    const result = exec(input).catch((err) => { throw err; });
+    const output = result.stdout ? `**\`OUTPUT\`**${"```sh"}\n${result.stdout}\n${"```"}` : "";
+    const outerr = result.stderr ? `**\`ERROR\`**${"```sh"}\n${result.stderr}\n${"```"}` : "";
+    const output2 = result.stdout ? `${"```sh"}\n${result.stdout}\n${"```"}` : "Done.";
+    const outerr2 = result.stderr ? `${"```sh"}\n${result.stderr}\n${"```"}\n` : "";
+    const embed = new Discord.RichEmbed()
+    .setColor("0x2ECC71")
+    .setAuthor('Exec / Shell', msg.author.avatarURL)
+    .setTimestamp()
+    .addField(":inbox_tray: Input", `\`\`\`sh\n${input}\n\`\`\``)
+    .addField(":outbox_tray: Output", `${output2}\n${outerr2}`)
+    .setFooter(msg.author.tag, msg.author.avatarURL);
+    return msg.channel.send({ embed });
+};
