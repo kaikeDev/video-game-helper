3024c66b74f0a6d9df7a23299a1931ba2afa0fc7
PillGP <gaming.bot.discord@gmail.com>
2018-02-12 15:10:37 +0000

    :bug: | Fixed League of Legends rotation

--- commands/lol.js
+++ commands/lol.js
@@ -28,12 +28,10 @@ exports.run = (client, msg, args) => {
                     return obj[key];
                 });
                 await $('.champion-icon').each(async function (i, element) {
-                    if (i < 14) {
                         let champ = element.attribs["data-champion"];
                         let c = allchamps.find(x=>x.name == champ)
                         let role = c? c.tags? c.tags[0]:"Specialist":"Specialist";
                         await rotation.push([emoji(role),champ]);
-                    }
                 });
             } if(error) {
                 await msg.channel.startTyping();
@@ -48,8 +46,8 @@ exports.run = (client, msg, args) => {
                 await emb.setAuthor('League of Legends', 'https://vignette1.wikia.nocookie.net/leagueoflegends/images/1/12/League_of_Legends_Icon.png/revision/latest?cb=20150402234343', 'http://leagueoflegends.wikia.com/wiki/League_of_Legends_Wiki');
                 await emb.setThumbnail("https://vignette1.wikia.nocookie.net/leagueoflegends/images/1/12/League_of_Legends_Icon.png/revision/latest?cb=20150402234343");
                 await emb.setDescription('Weekly Champion Rotation');
-                await emb.addField("₪₪₪₪₪₪₪₪₪₪₪", `${rotation[0][0]}  ${rotation[0][1]}\n${rotation[1][0]}  ${rotation[1][1]}\n${rotation[2][0]}  ${rotation[2][1]}\n${rotation[3][0]}  ${rotation[3][1]}\n${rotation[4][0]}  ${rotation[4][1]}\n${rotation[5][0]}  ${rotation[5][1]}\n${rotation[6][0]}  ${rotation[6][1]}`, true);
-                await emb.addField("₪₪₪₪₪₪₪₪₪₪₪", `${rotation[7][0]}  ${rotation[7][1]}\n${rotation[8][0]}  ${rotation[8][1]}\n${rotation[9][0]}  ${rotation[9][1]}\n${rotation[10][0]}  ${rotation[10][1]}\n${rotation[11][0]}  ${rotation[11][1]}\n${rotation[12][0]}  ${rotation[12][1]}\n${rotation[13][0]}  ${rotation[13][1]}`, true);
+                await emb.addField("₪₪₪₪₪₪₪₪₪₪₪", `${rotation[0][0]}  ${rotation[0][1]}\n${rotation[2][0]}  ${rotation[2][1]}\n${rotation[4][0]}  ${rotation[4][1]}\n${rotation[6][0]}  ${rotation[6][1]}\n${rotation[8][0]}  ${rotation[8][1]}\n${rotation[10][0]}  ${rotation[10][1]}\n${rotation[12][0]}  ${rotation[12][1]}`, true);
+                await emb.addField("₪₪₪₪₪₪₪₪₪₪₪", `${rotation[14][0]}  ${rotation[14][1]}\n${rotation[16][0]}  ${rotation[16][1]}\n${rotation[18][0]}  ${rotation[18][1]}\n${rotation[20][0]}  ${rotation[20][1]}\n${rotation[22][0]}  ${rotation[22][1]}\n${rotation[24][0]}  ${rotation[24][1]}\n${rotation[26][0]}  ${rotation[26][1]}`, true);
                 await emb.setFooter(msg.author.tag, msg.author.avatarURL);
                 await msg.channel.send({embed:emb});
                 await msg.channel.stopTyping();
